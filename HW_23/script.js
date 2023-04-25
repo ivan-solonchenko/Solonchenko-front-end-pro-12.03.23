baaa5a40039f786bf733fd9b0e9e6b3b9e204eba
Solonchenko Ivan <vanek.solonchenko@gmail.com>
2023-04-25 09:52:34 +0300

    added HW_23

---
+++ HW_23/script.js
@@ -0,0 +1,5 @@
+//За допомогою prompt запитати ім'я користувача.
+const userName = prompt('Введіть ваше ім\'я.');
+
+//За допомогою alert вивести "Hello, John! How are you?" , де John це те, що ввів користувач
+alert(`Hello, ${userName}! How are you?`);
