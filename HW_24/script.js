23d9ab3717411d1f81b9e281cc671a91d172340b
Solonchenko Ivan <vanek.solonchenko@gmail.com>
2023-04-27 18:46:51 +0300

    fixed HW_24

--- HW_24/script.js
+++ HW_24/script.js
@@ -58,7 +58,7 @@ class Hamburger {
 }
 
 // маленький гамбургер з начинкою з сиру
-var hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
 
 // добавка з майонезу
 hamburger.addTopping(Hamburger.TOPPING_MAYO);
